b274c4cfdbe5ed0e458391e24d78efc8fa9cce88
Bogdan Stefoni <bogdanstefoni.wd@gmail.com>
2020-11-05 16:50:04 +0200

    first commit

---
+++ src/test/java/com/bogdan/webchat/WebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdan.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
